8aad1cb6cf6289eca51d1edd628394250e12b3f0
Jam Nguyen <jam@jamnguyen.com>
2018-09-03 15:00:11 +0700

    Finalize project.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 class About extends Component {
   render() {
@@ -7,10 +6,10 @@ class About extends Component {
       <div>
         <h1 className="display-5">About Contact Manager</h1>
         <p>
-          Simple React app to manage contacts based on Udemy course <a target="_blank" href="https://www.udemy.com/react-front-to-back/">React Front To Back</a> by Brad Traversy.
+          Simple React app to manage contacts based on Udemy course <a target="_blank" rel="noopener noreferrer" href="https://www.udemy.com/react-front-to-back/">React Front To Back</a> by Brad Traversy.
           <br/>
           <br/>
-          <a target="_blank" href="https://github.com/jamnguyen/contact-manager"><i className="fas fa-angle-right" />  Github Repository</a>
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/jamnguyen/contact-manager"><i className="fas fa-angle-right" />  Github Repository</a>
           <br/>
           <br/>
         </p>
@@ -20,9 +19,11 @@ class About extends Component {
         <div>- Controlled Form</div>
         <div>- Routing</div>
 
-        <div className="lead font-bold mt-3">Using</div>
+        <div className="lead font-bold mt-3">Packages used</div>
         <div>- React Router DOM</div>
         <div>- Bootstrap</div>
+        <div>- Classnames</div>
+        <div>- UUID</div>
       </div>
     )
   }
